311c482f97d73b6ab2f34cfaf29525cd029adb7c
neoys97 <neoys97@gmail.com>
2020-01-09 12:14:05 +0800

    Initial Commit

---
+++ TimeTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTracker
+//
+//  Created by Neo Yi Siang on 9/1/2020.
+//  Copyright © 2020 Neo Yi Siang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
